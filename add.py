c06e778a77c2e5f746ec5a4e0b620226dee2c84b
kennnedytz <mrkennito@gmail.com>
2025-08-20 15:44:33 +0300

    added a code

---
+++ add.py
@@ -0,0 +1,25 @@
+#A program to add two numbers
+a = 10
+b = 20
+sum = a + b
+print (f"the sum of {a} and {b} is {sum}")
+
+'''
+adding two numbers using user input
+'''
+num_1 = int(input("enter first number"))
+num_2 = int (input("enter second number"))
+sum = num_1 + num_2
+print(f"sum of {num_1} and {num_2} is {sum}")
+num_1 = int(input("enter first number"))
+num_2 = int (input("enter second number"))
+product = num_1 * num_2
+print(f"product of {num_1} and {num_2} is {product}")
+num_1 = int(input("enter first number"))
+num_2 = int (input("enter second number"))
+divident = num_1 / num_2
+print(f"divident of {num_1} and {num_2} is {divident}")
+num_1 = int(input("enter first number"))
+num_2 = int (input("enter second number"))
+difference = num_1 - num_2
+print(f"difference between {num_1} and {num_2} is {difference}")
